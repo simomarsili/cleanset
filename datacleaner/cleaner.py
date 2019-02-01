d91362075e5ec0abafbd993ad23c3ab2713c02e8
simomarsili <simomarsili@gmail.com>
2019-02-01 17:12:38 +0100

    smart update statistics

--- datacleaner/cleaner.py
+++ datacleaner/cleaner.py
@@ -41,6 +41,9 @@ class Cleaner(BaseEstimator, TransformerMixin, EntrySelector):
     def __init__(self, na='NA', thr=0.1):
         super().__init__(condition=na)
         self.thr = thr
+        self.records_ = None
+        self.fields_ = None
+        self.mask_ = None
 
     def fit(self, X, y=None):
         """Compute the mean and std to be used for later scaling.
@@ -59,9 +62,9 @@ class Cleaner(BaseEstimator, TransformerMixin, EntrySelector):
         mask = self.mask(X)
 
         n1, p1 = n, p
+        ng0 = mask.sum(axis=0)  # p-dimensional (columns)
+        ng1 = mask.sum(axis=1)  # n-dimensional (rows)
         while 1:
-            ng0 = mask.sum(axis=0)  # p-dimensional (columns)
-            ng1 = mask.sum(axis=1)  # n-dimensional (rows)
 
             r = numpy.argmax(ng1)  # index of most gappy row
             c = numpy.argmax(ng0)  # index of most gappy column
@@ -71,6 +74,9 @@ class Cleaner(BaseEstimator, TransformerMixin, EntrySelector):
 
             if nr <= p1 * self.thr and nc <= n1 * self.thr:
                 self.records_, self.fields_ = records, fields
+                records = [1 if x in records else 0 for x in range(n)]
+                fields = [1 if x in fields else 0 for x in range(p)]
+                self.mask_ = numpy.outer(records, fields)
                 return self
             else:
                 if len(records) % 100 == 0:
@@ -79,13 +85,21 @@ class Cleaner(BaseEstimator, TransformerMixin, EntrySelector):
                 # remove a column
                 p1 -= 1
                 fields.remove(c)
-                mask[:, c] = False
+                ng0[c] = 0
                 ng1 -= mask[:, c]
+                mask[:, c] = False
                 # ali = numpy.delete(ali, cmax, axis=1)
             else:
                 n1 -= 1
                 # remve a row
                 records.remove(r)
-                mask[r] = False
                 ng0 -= mask[r]
+                ng1[r] = 0
+                mask[r] = False
             # ali = numpy.delete(ali, rmax, axis=0)
+
+    def transform(self, X):
+        if self.records_ is not None:
+            return X[self.records_][:, self.fields_]
+        else:
+            raise ValueError('This istance is Not fitted yet.')
